8566205ce7aa4467ea24cfd542ae906324eea1e3
shafaiatul islam <shafaiatul@gmail.com>
2015-12-22 00:46:43 -0500

    Authenticated users with angularFire

---
+++ app/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular
+	.module('gearshellchat', ['firebase', 'angular-md5', 'ui.router'])
+	
+	.config(function ($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: 'home/home.html'
+			})
+			.state('login', {
+				url: '/login',
+				controller: 'AuthCtrl as authCtrl',
+				templateUrl: 'auth/login.html',
+				resolve: {
+					requireNoAuth: function ($state, Auth) {
+						return Auth.$requireAuth().then(function (auth) {
+							$state.go('home')
+						}, function (error) {
+							return;
+						})
+					}
+				}
+			})
+			.state('register', {
+				url: '/register',
+				controller: 'AuthCtrl as authCtrl',
+				templateUrl: 'auth/register.html',
+				resolve: {
+					requireNoAuth: function ($state, Auth) {
+						return Auth.$requireAuth().then(function (auth) {
+							$state.go('home')
+						}, function (error) {
+							return;
+						})
+					}
+				}
+			});
+
+		$urlRouterProvider.otherwise('/');
+	})
+	.constant('FirebaseUrl', 'https://gearshellchat.firebaseio.com/');
